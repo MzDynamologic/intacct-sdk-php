f8aa525d28547bcab95df6300ee408bc8342c843
Moazzam Ali <Moazzam.Ali@Dynamologic.com>
2022-09-21 19:53:40 +0500

    Implement Get PDF API call as a generic class

---
+++ src/Intacct/Functions/Common/GetPdf.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Copyright 2021 Sage Intacct, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Intacct\Functions\Common;
+
+use Intacct\Functions\AbstractFunction;
+use Intacct\Xml\XMLWriter;
+
+class GetPdf extends AbstractFunction
+{
+    /** @var string */
+    protected $objectname;
+
+    /**
+     * @return string
+     */
+    public function getObjectname(): string
+    {
+        return $this->objectname;
+    }
+
+    /**
+     * @param string $objectname
+     */
+    public function setObjectname(string $objectname): void
+    {
+        $this->objectname = $objectname;
+    }
+
+    public function writeXml(XMLWriter &$xml)
+    {
+        $xml->startElement('function');
+        $xml->writeAttribute('controlid', $this->controlId, true);
+        $xml->startElement('retrievepdf');
+        $xml->startElement($this->getObjectname());
+        if ($this->getRecordNo()) {
+            $element_name = ($this->getObjectname() == "ARINVOICE")? "RECORDNO" : "DOCID";
+            $xml->writeElement($element_name, $this->getRecordNo());
+        } else {
+            throw new InvalidArgumentException('Record No is required for getting PDF');
+        }
+        $xml->endElement(); // object name
+        $xml->endElement(); // retrieve pdf
+        $xml->endElement(); // function
+    }
+}
